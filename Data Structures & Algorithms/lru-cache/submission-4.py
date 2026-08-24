041323dea7ff87711c5e4adc09369827973e7516
Ethan Chen <chen.ethan529@gmail.com>
2026-08-24 17:59:15 -0500

    Add: lru-cache - submission-4

---
+++ Data Structures & Algorithms/lru-cache/submission-4.py
@@ -0,0 +1,29 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.items = {}
+        self.capacity = capacity
+
+    def get(self, key: int) -> int:
+        if key not in self.items:
+            return -1
+        
+        value = self.items.pop(key)
+        self.items[key] = value
+
+        return value
+        
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.items:
+            del self.items[key]
+
+        self.items[key] = value
+
+        if len(self.items) > self.capacity:
+            del self.items[next(iter(self.items))]
+        
+        
+
+
+        
