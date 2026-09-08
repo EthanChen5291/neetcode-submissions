0b0e500240c92f97bd5e291aea6511b18bed6d09
Ethan Chen <chen.ethan529@gmail.com>
2026-09-08 15:05:46 -0500

    Add: time-based-key-value-store - submission-8

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-8.py
@@ -0,0 +1,33 @@
+class TimeMap:
+
+    def __init__(self):
+        self.store = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.store:
+            self.store[key] = []
+
+        self.store[key].append((timestamp, value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.store:
+            return ""
+
+        vals = self.store[key]
+
+        left = 0
+        right = len(vals) - 1
+        res = ""
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            currTimestamp = vals[mid][0]
+
+            if currTimestamp <= timestamp:
+                res = vals[mid][1]
+                left = mid + 1   # try to find an even later valid timestamp
+            else:
+                right = mid - 1
+
+        return res
